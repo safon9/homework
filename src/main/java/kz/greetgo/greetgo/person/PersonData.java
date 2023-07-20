4a38a1c4de02ed541f230684178c9099781efc15
safon9 <safon9@list.ru>
2023-07-20 17:39:17 +0600

    entity and properties

---
+++ src/main/java/kz/greetgo/greetgo/person/PersonData.java
@@ -0,0 +1,52 @@
+package kz.greetgo.greetgo.person;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "person_data", schema = "personData", catalog = "postgres")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonData {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "birth_date")
+    private Date birthDate;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "secondary_number")
+    private String secondaryPhoneNumber;
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonData personData = (PersonData) o;
+        return Objects.equals(id, personData.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
