a6b3a2ec559e49946c0f1f57be007dcca119c98e
safon9 <safon9@list.ru>
2023-07-20 16:42:43 +0600

    init commit

---
+++ src/main/java/kz/greetgo/greetgo/GreetgoApplication.java
@@ -0,0 +1,13 @@
+package kz.greetgo.greetgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/greetgo/greetgo/GreetgoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.greetgo.greetgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
